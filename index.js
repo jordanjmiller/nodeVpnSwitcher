c997530ceffa7ef50de45b8d5e7b6b9500fa4dbb
Jordan Miller <jordan.j.miller@live.com>
2020-10-15 14:17:54 -0700

    Added firing clogs and updated function names

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const macLinuxPIAString = 'piactl';
 const loginFile = ".\\creds.txt";
 let regions = '';
 
-const getRegions = () => {
+const piaGetRegions = () => {
+    console.log('piaGetRegions firing');
     exec(`${windowsPIAString} get regions`, (error, stdout, stderr) => {
-        if (error) { console.log(`getRegions error: ${error.message}`); return false; }
-        if (stderr) { console.log(`getRegions stderr: ${stderr}`); return false; }
+        if (error) { console.log(`piaGetRegions error: ${error.message}`); return false; }
+        if (stderr) { console.log(`piaGetRegions stderr: ${stderr}`); return false; }
         const stdArray = stdout.split("\n");
 
         // remove 'auto' as an option
@@ -17,21 +18,23 @@ const getRegions = () => {
         if (index > -1) { stdArray.splice(index, 1); }
 
         regions = [...stdArray];
-        console.log(`getRegions success, ${regions.length} regions found`);
+        console.log(`piaGetRegions success, ${regions.length} regions found`);
         return true;
     });
 }
 
-const enableBackground = () => {
+const piaEnableBackground = () => {
+    console.log('piaEnableBackground firing');
     exec(`${windowsPIAString} background enable`, (error, stdout, stderr) => {
-        if (error) { console.log(`enableBackground error: ${error.message}`); return false; }
-        if (stderr) { console.log(`enableBackground stderr: ${stderr}`); return false; }
-        console.log('enableBackground success, stdout:', stdout);
+        if (error) { console.log(`piaEnableBackground error: ${error.message}`); return false; }
+        if (stderr) { console.log(`piaEnableBackground stderr: ${stderr}`); return false; }
+        console.log('piaEnableBackground success, stdout:', stdout);
         return true;
     });
 }
 
 const piaLogin = () => {
+    console.log('piaLogin firing');
     exec(`${windowsPIAString} login ${loginFile}`, (error, stdout, stderr) => {
         if (error) { 
             if (error.message.includes('Already logged into account')){ console.log('piaLogin: Already logged into PIA'); return true; }
@@ -45,10 +48,13 @@ const piaLogin = () => {
 
 const piaSetup = () => {
     //check if logged in, log in if not
-    piaLogin();
-    //enable PIA running in the background without the PIA GUI running
-    //get array of regions
-    //
+    if (piaLogin()){
+        //enable PIA running in the background without the PIA GUI running
+        //get array of regions
+        //connect to first region
+        //verify connection status and return
+    }
+    else { console.log('piaSetup error: not logged in'); }
 }
 
 piaSetup();
