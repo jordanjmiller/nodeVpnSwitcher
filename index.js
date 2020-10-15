174831fbbd7c52ff245d7453a154ffc04e459b6a
Jordan Miller <jordan.j.miller@live.com>
2020-10-15 15:19:47 -0700

    Converted piaGetRegions() and piaBackgroundEnable() to return promises

--- index.js
+++ index.js
@@ -8,28 +8,51 @@ let regions = '';
 
 const piaGetRegions = () => {
     console.log('piaGetRegions firing');
-    exec(`${windowsPIAString} get regions`, (error, stdout, stderr) => {
-        if (error) { console.log(`piaGetRegions error: ${error.message}`); return false; }
-        if (stderr) { console.log(`piaGetRegions stderr: ${stderr}`); return false; }
-        const stdArray = stdout.split("\n");
+    // exec(`${windowsPIAString} get regions`, (error, stdout, stderr) => {
+    //     if (error) { console.log(`piaGetRegions error: ${error.message}`); return false; }
+    //     if (stderr) { console.log(`piaGetRegions stderr: ${stderr}`); return false; }
+    //     const stdArray = stdout.split("\n");
 
-        // remove 'auto' as an option
-        const index = stdArray.indexOf('auto');
-        if (index > -1) { stdArray.splice(index, 1); }
+    //     // remove 'auto' as an option
+    //     const index = stdArray.indexOf('auto');
+    //     if (index > -1) { stdArray.splice(index, 1); }
 
-        regions = [...stdArray];
-        console.log(`piaGetRegions success, ${regions.length} regions found`);
-        return true;
+    //     regions = [...stdArray];
+    //     console.log(`piaGetRegions success, ${regions.length} regions found`);
+    //     return true;
+    // });
+    return new Promise(function(resolve, reject) { 
+        exec(`${windowsPIAString} get regions`, (error, stdout, stderr) => {
+            if (error) { console.log(`piaGetRegions error: ${error.message}`); reject(error.message); return; }
+            if (stderr) { console.log(`piaGetRegions stderr: ${stderr}`); reject(stderr); return; }
+            const stdArray = stdout.split("\n");
+    
+            // remove 'auto' as an option
+            const index = stdArray.indexOf('auto');
+            if (index > -1) { stdArray.splice(index, 1); }
+            regions = [...stdArray];
+
+            console.log(`piaGetRegions success, ${regions.length} regions found`);
+            resolve(true);
+        });
     });
 }
 
 const piaEnableBackground = () => {
     console.log('piaEnableBackground firing');
-    exec(`${windowsPIAString} background enable`, (error, stdout, stderr) => {
-        if (error) { console.log(`piaEnableBackground error: ${error.message}`); return false; }
-        if (stderr) { console.log(`piaEnableBackground stderr: ${stderr}`); return false; }
-        console.log('piaEnableBackground success, stdout:', stdout);
-        return true;
+    // exec(`${windowsPIAString} background enable`, (error, stdout, stderr) => {
+    //     if (error) { console.log(`piaEnableBackground error: ${error.message}`); return false; }
+    //     if (stderr) { console.log(`piaEnableBackground stderr: ${stderr}`); return false; }
+    //     console.log('piaEnableBackground success, stdout:', stdout);
+    //     return true;
+    // });
+    return new Promise(function(resolve, reject) { 
+        exec(`${windowsPIAString} background enable`, (error, stdout, stderr) => {
+            if (error) { console.log(`piaEnableBackground error: ${error.message}`); reject(error.message); return; }
+            if (stderr) { console.log(`piaEnableBackground stderr: ${stderr}`); reject(stderr); return; }
+            console.log('piaEnableBackground success, stdout:', stdout);
+            resolve(true);
+        });
     });
 }
 
@@ -52,7 +75,7 @@ const piaConnect = (region) => {
     console.log('piaConnect firing, region:', region);
     return new Promise(function(resolve, reject) { 
         exec(`${windowsPIAString} connect ${region}`, (error, stdout, stderr) => {
-            if (error) { console.log(`piaConnect error: ${error.message}`); return false; }
+            if (error) { console.log(`piaConnect error: ${error.message}`); reject(error.message); return; }
             if (stderr) { console.log(`piaConnect stderr: ${stderr}`); reject(stderr); return; }
             console.log('piaConnect success, stdout:', stdout);
             resolve(true);
@@ -64,9 +87,9 @@ const piaSetup = async () => {
     //check if logged in, log in if not
     if (await piaLogin()){
         //enable PIA running in the background without the PIA GUI running
-        piaEnableBackground();
+        await piaEnableBackground();
         //get array of regions
-        piaGetRegions();
+        await piaGetRegions();
         //connect to first region
         await piaConnect(regions[0]);
         //verify connection status and return
