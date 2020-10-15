77786a54dd0e7faaf63a3b72683543e48af8b789
Jordan Miller <jordan.j.miller@live.com>
2020-10-15 11:48:50 -0700

    Added piaLogin() and updated gitignore to ignore creds.txt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { exec } = require("child_process");
 //Must escape your \'s by adding a second one, and wrap everything in a second string to preserve spaces/treat as one complete string
 const windowsPIAString = '"C:\\Program Files\\Private Internet Access\\piactl.exe"'; 
 const macLinuxPIAString = 'piactl';
+const loginFile = ".\\creds.txt";
 let regions = '';
 
 exec(`${windowsPIAString} get regions`, (error, stdout, stderr) => {
@@ -52,6 +53,21 @@ const enableBackground = () => {
             return false;
         }
         console.log('enableBackground success, stdout:', stdout);
+        return true;
+    });
+}
+
+const piaLogin = () => {
+    exec(`${windowsPIAString} login ${loginFile}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return false;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return false;
+        }
+        console.log('login success, stdout:', stdout);
         return true;
     });
 }
